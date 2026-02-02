49ca0925829ab370968e99c59688c23a74549aad
Bhavy <bhavyajain817@gmail.com>
2026-02-02 16:04:49 +0530

    changes

--- src/app/partners/searchResults/page.tsx
+++ src/app/partners/searchResults/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { collection, query, where, orderBy, getDocs, Timestamp, updateDoc, arrayUnion } from 'firebase/firestore';
